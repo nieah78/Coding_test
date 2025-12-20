d09a03cbe52d89bd16fa31652817a8a9dbf27f10
nieah78 <150982810+nieah78@users.noreply.github.com>
2025-12-20 23:44:18 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    char A[4];
+    char B[4];
+    scanf("%s %s", A, B);
+    int a, b;
+
+    a = (A[2]-'0')*100 + (A[1]-'0')*10 + A[0]-'0';
+    b = (B[2]-'0')*100 + (B[1]-'0')*10 + B[0]-'0';
+    
+    if(a > b){
+        printf("%d", a);
+    }
+    else{
+        printf("%d", b);
+    }
+}
