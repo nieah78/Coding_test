e495f654c8d0ed995ca532acd7125a81cccec4f4
nieah78 <150982810+nieah78@users.noreply.github.com>
2025-12-22 23:51:17 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T, A, B;
+    cin>>T;
+    int arr[T];
+    for(int i=0; i<T; i++){
+        cin>>A>>B;
+        arr[i] = A+B;
+    }
+    for(int i=0; i<T; i++){
+        cout<<arr[i]<<endl;
+    }
+
+}
