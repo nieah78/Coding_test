d2d89965370dff244588c257d2aba87f63311113
nieah78 <150982810+nieah78@users.noreply.github.com>
2026-01-05 17:11:26 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 232 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T, a, b;
+    cin>>T;
+    for(int i = 0; i < T; i++){
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+}
