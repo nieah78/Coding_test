d812856e95e56e4fa9014308d796d7eba857c09f
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-03-08 23:16:03 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1524 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    int count[10001] = {0};
+    int number = 0;
+
+    for(int i = 0; i < N; i++){
+        cin >> number;
+        count[number]++;
+    }
+
+    for(int i = 0; i <= 10000; i++){
+        if(count[i] != 0)
+            for(int j = 0; j < count[i]; j++)
+                cout << i << "\n";
+    }
+}
