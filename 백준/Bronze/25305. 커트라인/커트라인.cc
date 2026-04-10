65a20fc51c2e96264b546f2a0868f39bc60445f4
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-04-10 20:18:36 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+static int N, k;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> k;
+    vector<int> v(N);
+    int num;
+
+    for(int i = 0; i < N; i++){
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    cout << v[N-k];    
+    return 0;
+}
