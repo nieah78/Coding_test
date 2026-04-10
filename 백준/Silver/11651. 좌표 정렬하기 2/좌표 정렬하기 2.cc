3702bfe01de1c268c72660250cb665997e10c76a
Sujin Hong <150982810+nieah78@users.noreply.github.com>
2026-04-11 01:54:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> v(N);
+    for(int i = 0; i < N; i++){
+        cin >> v[i].second >> v[i].first;
+    }
+
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < N; i++){
+        cout << v[i].second << " " << v[i].first << "\n";
+    }
+    return 0;
+}
